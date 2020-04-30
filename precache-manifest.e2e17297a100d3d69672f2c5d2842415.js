7f43c716b66bac7f84010fe3ae71e21dbef351fd
Bogdan Kondrenko <kondrenkobogdan2808@gmail.com>
2020-04-30 12:03:20 +0300

    Updates

---
+++ precache-manifest.e2e17297a100d3d69672f2c5d2842415.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1534d3e5015ab8fabb4859c0125c29",
+    "url": "/create-react-app-gh-pages/index.html"
+  },
+  {
+    "revision": "f3cedcede4846e91e33b",
+    "url": "/create-react-app-gh-pages/static/css/4.37e7d950.chunk.css"
+  },
+  {
+    "revision": "38ce7b34bb3436980a12",
+    "url": "/create-react-app-gh-pages/static/css/5.15e655ae.chunk.css"
+  },
+  {
+    "revision": "26a7e52089f94f288363",
+    "url": "/create-react-app-gh-pages/static/css/6.71b11586.chunk.css"
+  },
+  {
+    "revision": "f9b7f93790f5fdbcc221",
+    "url": "/create-react-app-gh-pages/static/css/7.cf5127ef.chunk.css"
+  },
+  {
+    "revision": "0dc875fab20428197061",
+    "url": "/create-react-app-gh-pages/static/css/8.9505f6d0.chunk.css"
+  },
+  {
+    "revision": "8f6b0f3b9eb9db89a1ba",
+    "url": "/create-react-app-gh-pages/static/css/main.bf706a53.chunk.css"
+  },
+  {
+    "revision": "e238e82e27c0ea3690cb",
+    "url": "/create-react-app-gh-pages/static/js/0.8c7122f9.chunk.js"
+  },
+  {
+    "revision": "656049767a40c6861bd9",
+    "url": "/create-react-app-gh-pages/static/js/3.3f88d6cb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/create-react-app-gh-pages/static/js/3.3f88d6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3cedcede4846e91e33b",
+    "url": "/create-react-app-gh-pages/static/js/4.256dd54f.chunk.js"
+  },
+  {
+    "revision": "38ce7b34bb3436980a12",
+    "url": "/create-react-app-gh-pages/static/js/5.cc25f5e1.chunk.js"
+  },
+  {
+    "revision": "26a7e52089f94f288363",
+    "url": "/create-react-app-gh-pages/static/js/6.798b9794.chunk.js"
+  },
+  {
+    "revision": "f9b7f93790f5fdbcc221",
+    "url": "/create-react-app-gh-pages/static/js/7.afecc67b.chunk.js"
+  },
+  {
+    "revision": "0dc875fab20428197061",
+    "url": "/create-react-app-gh-pages/static/js/8.e102162a.chunk.js"
+  },
+  {
+    "revision": "8f6b0f3b9eb9db89a1ba",
+    "url": "/create-react-app-gh-pages/static/js/main.bf2b2b5a.chunk.js"
+  },
+  {
+    "revision": "091a55e1b3285a16f91f",
+    "url": "/create-react-app-gh-pages/static/js/runtime-main.31f97886.js"
+  },
+  {
+    "revision": "73066d3ea1201370c6850ed8f73331aa",
+    "url": "/create-react-app-gh-pages/static/media/480 (2).73066d3e.gif"
+  },
+  {
+    "revision": "ad0f429318e50fbaae53aa0708f7db29",
+    "url": "/create-react-app-gh-pages/static/media/Cartoon_Robot.svg.ad0f4293.png"
+  },
+  {
+    "revision": "716105d1a59e72154b02eeec495c7bb5",
+    "url": "/create-react-app-gh-pages/static/media/avatar.716105d1.png"
+  },
+  {
+    "revision": "1e8159842479027a394113fe9c23673b",
+    "url": "/create-react-app-gh-pages/static/media/cover.1e815984.png"
+  }
+]);
